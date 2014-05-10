ef109afcbf5e0def2f1ecf2ca6a03a291b7581e9
Deric Cheng <dericc@princeton.edu>
2014-05-10 17:05:29 -0400

    Fixed that one file. Player is broken in new scene.

--- src/R3Scene.h
+++ src/R3Scene.h
@@ -180,6 +180,7 @@ struct R3Scene {
   // I/O functions
   int Read(const char *filename, R3Node *root = NULL);
 
+  void WritePlayer(FILE *fp); 
   void WriteMaterials(FILE *fp); 
   void WriteNode(FILE *fp, R3Node *node); 
   int Write(const char *filename, R3Node *node); 
