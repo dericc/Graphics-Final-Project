fa4e08d9e0eeed69b67d35bfddde5400b92efd21
Deric Cheng <dericc@princeton.edu>
2014-05-10 16:42:32 -0400

    Working output new scene file.

--- src/game.cpp
+++ src/game.cpp
@@ -21,8 +21,6 @@
 
 static const double VIDEO_FRAME_DELAY = 1./25.; // 25 FPS 
 
-
-
 ////////////////////////////////////////////////////////////
 // GLOBAL VARIABLES
 ////////////////////////////////////////////////////////////
@@ -219,6 +217,8 @@ void CollidePlayer(R3Node *node)
 
 void UpdatePlayer(R3Scene *scene) {
   R3Player *p = scene->player;
+
+  if (p == NULL) return; 
   // Get current time (in seconds) since start of execution
   double current_time = GetTime();
   static double previous_time = 0;
@@ -1343,6 +1343,8 @@ ReadScene(const char *filename)
     fprintf(stderr, "Unable to read scene from %s\n", filename);
     return NULL;
   }
+
+  scene->Write(filename, scene->root); 
 
   // Remember initial camera
   camera = scene->camera;
