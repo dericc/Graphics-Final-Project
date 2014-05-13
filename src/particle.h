90430df37053f91d267640eb10a490a7f7b892eb
Riley Thomasson <riley.thomasson@gmail.com>
2014-05-13 05:09:45 -0400

    added death particles, stopped updating player on death

--- src/particle.h
+++ src/particle.h
@@ -19,6 +19,7 @@ void UpdateParticles(R3Scene *scene, double current_time, double delta_time, int
 void EulerStep(R3Scene *scene, double time_step, vector<R3Point> *init_pos=NULL, vector<R3Vector> *init_vel=NULL);
 R3Vector ComputeForce(R3Scene *scene, R3Particle *particle);
 void GenerateParticles(R3Scene *scene, double current_time, double delta_time);
-void RenderParticles(R3Scene *scene, double current_time, double delta_time);
+void RenderParticles(R3Scene *scene);
+void CreateParticles(R3Scene *scene, R3Point position, int count, R3Material *mat, double velocity);
 
 
